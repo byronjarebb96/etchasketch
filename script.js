baf2c50245ddfa8f0cfe905ca0c838d0543e1382
Byron <133577261+byronjarebb96@users.noreply.github.com>
2023-06-23 18:43:29 +0400

    Add row and column input prompts

--- script.js
+++ script.js
@@ -2,19 +2,33 @@ const container = document.querySelector("#container");
 function transformSquare(e){
     e.target.classList.add('transformedSquare');
 
-}
 
+}
+function resetBoard(){
+    squares.forEach(sq => sq.classList.remove('transformedSquare'))
+}
 let rows = 48;
 let columns = 48;
+function generateBoard(rows=16, columns=16){
+    rows = prompt("Set rows:");
+    columns = prompt("Set columns:");
+    for (let i = 0; i < (rows * columns); i++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${(600/rows)}px`;
+        square.style.height = `${(600/columns)}px`;
+        container.appendChild(square);
+        squares = document.querySelectorAll('.square');
+        squares.forEach(sq => sq.addEventListener('mouseenter', transformSquare))
 
-for (let i = 0; i < (rows * columns); i++){
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.width = `${(600/rows)}px`;
-    square.style.height = `${(600/columns)}px`;
-    container.appendChild(square);
+    }    
+
 }
-
-
-const squares = document.querySelectorAll('.square');
+let squares = document.querySelectorAll('.square');
 squares.forEach(sq => sq.addEventListener('mouseenter', transformSquare))
+
+const setupBtn = document.querySelector('#setup');
+setupBtn.addEventListener('click', () => generateBoard())
+
+const button = document.querySelector('#reset');
+button.addEventListener('click', () => resetBoard());
