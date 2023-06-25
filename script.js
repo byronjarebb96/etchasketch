85eda1f3592aadc2cff6b86f2387b278e3c0505e
Byron <133577261+byronjarebb96@users.noreply.github.com>
2023-06-25 18:18:35 +0400

    Fixed primary setup, add border-box sizing

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+const body = document.querySelector('body')
 const container = document.querySelector("#container");
 function transformSquare(e){
     e.target.classList.add('transformedSquare');
-
 
 }
 function resetBoard(){
@@ -9,14 +9,23 @@ function resetBoard(){
 }
 let rows = 48;
 let columns = 48;
-function generateBoard(rows=16, columns=16){
+function setupBoard(){
     rows = prompt("Set rows:");
     columns = prompt("Set columns:");
+    if(!squares==0){
+        squares.forEach(sq => sq.remove())
+
+    }
+    generateBoard(rows, columns)
+}
+function generateBoard(rows=16, columns=16){
+ 
     for (let i = 0; i < (rows * columns); i++){
         const square = document.createElement('div');
         square.classList.add('square');
         square.style.width = `${(600/rows)}px`;
         square.style.height = `${(600/columns)}px`;
+        //square.textContent = `${i+1}`;
         container.appendChild(square);
         squares = document.querySelectorAll('.square');
         squares.forEach(sq => sq.addEventListener('mouseenter', transformSquare))
@@ -25,10 +34,12 @@ function generateBoard(rows=16, columns=16){
 
 }
 let squares = document.querySelectorAll('.square');
-squares.forEach(sq => sq.addEventListener('mouseenter', transformSquare))
+squares.forEach(sq => sq.addEventListener('click', transformSquare))
 
 const setupBtn = document.querySelector('#setup');
-setupBtn.addEventListener('click', () => generateBoard())
+setupBtn.addEventListener('click', () => setupBoard())
 
 const button = document.querySelector('#reset');
 button.addEventListener('click', () => resetBoard());
+
+generateBoard(16,16);
